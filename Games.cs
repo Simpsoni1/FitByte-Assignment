7b55384dbcdaa65ac6f3fc567e7274497a4bcd62
Simpsoni1 <isaiah.simpson@stmartin.edu>
2025-07-19 19:58:06 -0700

    finished the code. Just need to add comments.

--- Games.cs
+++ Games.cs
@@ -1,14 +1,39 @@
 public class Games
 {
-    public string name;
+    public Genre genre;
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    private string studio;
+    public string Studio
+    {
+        get { return studio; }
+        set { studio = value; }
+    }
 
+    private int totalplaytime;
+    private int playtime;
+    public void Play(int playtime)
+    {
+        this.playtime = playtime;
+        totalplaytime += this.playtime;
+    }
+    public string gamedetails()
+    {
+        return name + " " + genre + " Time played = " + totalplaytime + "min";
+    }
 }
 
 
-enum Genre
+
+
+public enum Genre
 {
-    Action,
+    Platformer,
     Puzzle,
-    Sports,
+    Roguelike,
     MMO
 }
