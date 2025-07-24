f8c8392438d26004dde5b86611f7f45809e49d39
Simpsoni1 <isaiah.simpson@stmartin.edu>
2025-07-24 08:28:22 -0700

    added comments to the code. FitByte assignment complete.

--- FitByte.cs
+++ FitByte.cs
@@ -1,12 +1,13 @@
 public class FitByte
 {
-    //private encapsolated variables
+    //private encapsolated variables.
     private int age;
     public int Age
     {
         get { return age; }
         set { age = value; }
     }
+    //private encapsolated variables.
     private int restingHR;
     public int RestingHR
     {
@@ -15,28 +16,32 @@ public class FitByte
     }
     private double targetHR;
     private double maxHR;
+    //Default values for the class.
     public FitByte()
     {
         age = 20;
         restingHR = 65;
 
     }
+    //Lets the user set their age and heart rate.
     public FitByte(int age, int restingHR)
     {
         this.age = age;
         this.restingHR = restingHR;
     }
+    //Calculates max heart reate when called.
     public double MaxHeartrate()
     {
         maxHR = 206.3 - (0.711 * age);
         return maxHR;
     }
-
+    //Callculate target heart rate when called.
     public double TargetHeartrate(double percentageOfMaximum)
     {
         targetHR = (maxHR - restingHR) * percentageOfMaximum + restingHR;
         return targetHR;
     }
+    //Displays both max and target heart rate when called.
     public void Display()
     {
         Console.WriteLine(maxHR + " BPM");
--- Games.cs
+++ Games.cs
@@ -1,13 +1,17 @@
 public class Games
 {
     public Genre genre;
+
     private string name;
+    //Gets the name of the game and sotres it in the private feild above.
     public string Name
     {
         get { return name; }
         set { name = value; }
     }
     private string studio;
+    //Gets the name of the studi and sotres it in the private feild above.
+    //not actual implimented
     public string Studio
     {
         get { return studio; }
@@ -16,20 +20,19 @@ public class Games
 
     private int totalplaytime;
     private int playtime;
+    //Adds the time played for the game and stores it.
     public void Play(int playtime)
     {
         this.playtime = playtime;
         totalplaytime += this.playtime;
     }
+    // Displays the details of the specified game.
     public string gamedetails()
     {
         return name + " " + genre + " Time played = " + totalplaytime + "min";
     }
 }
-
-
-
-
+//Diffrent posible game genres.
 public enum Genre
 {
     Platformer,
